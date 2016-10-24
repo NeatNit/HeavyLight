432f25fcc4def2905652f35bb3996dbe3f57d317
NeatNit <neatnit@gmail.com>
2016-10-24 22:14:32 +0300

    how slow am I?
    really slow

--- lua/includes/modules/heavylight.lua
+++ lua/includes/modules/heavylight.lua
@@ -1,43 +1,103 @@
 module("heavylight", package.seeall) -- package.seeall actually sucks, but whatever
 
-if SERVER then
-	-- what are you doing here?
-	AddCSLuaFile() -- I guess?
-	return
+assert(CLIENT, "Tried to include a client module (heavylight) in a server file!")
+
+do
+	local RTs = {}
+
+	function GetRT(rttype, index)
+		RTs[rttype] = RTs[rttype] or {}
+
+		if not RTs[rttype][index] then
+			RTs[rttype][index] = GetRenderTargetEx(
+				"HeavyLight" .. rttype .. index,	-- Name
+				ScrW(),	-- Width
+				ScrH(),	-- Height
+				RT_SIZE_DEFAULT,	-- Size Mode, does this even affect anything?
+				MATERIAL_RT_DEPTH_SHARED,	-- Depth Mode, what is this?
+				nil,	-- Texture Flags, shit documentation
+				0,	-- RT flags
+				_G["IMAGE_FORMAT_" .. rttype])	-- Image Format
+		end
+
+		return RTs[rttype][index]
+	end
+end
+
+local function GetAddRT(rttype, index)
+	rttype = rttype or "basic"
+	if isstring(rttype) then
+		if		rttype == "basic" then
+			index = math.floor((index or 0) / 257)
+			return RTs.RGBA16161616[index]
+		elseif	rttype == "int" then
+			-- RGBA16161616
+		elseif	rttype == "float" then
+			-- RGBA16161616F
+		end
+	elseif index >= 0 then
+		return rttype.Render
+	else
+		return rttype.Blend
+	end
 end
 
 --[[
 renderdef structure:
 {
-	NewSection = function to be called at the beginning of each section of the poster. Can be nil if this isn't needed.
-		For example, if poster_size is 3, new_section_func will be called 9 times.
+	Start = function to be called at the beginning of each section of the poster.
+		For example, if PosterSize is 3, Start will be called 9 times.
 		The appropriate render target will be set prior to this function being called.
-		This function will receive 3 parameters - poster_size, x coordinate, and y coordinate.
-	Tick = function to be called immediately after new_section_func, and repeatedly (with blends in between) as long as it returns true.
+		This function will receive these arguments - PosterSize, x coordinate, and y coordinate.
+		Optional.
+	Tick = function to be called immediately after Start, and repeatedly (with blends in between) as long as it returns true.
 		The appropriate render target will be set prior to this function being called.
-		This function will receive 4 parameters - poster_size, x coordinate, y coordinate, and tick count (per section).
+		This function will receive these arguments - tick number (running count per section), PosterSize, x coordinate, y coordinate
+		This function must return a boolean value. Optionally, it can additionally return extra data used to display progress bars:
+																																		-- FILL THIS IN
+	Finish = A function called after Tick returns false. When this function is finished, the blend texture is expected to hold the final image.
+		This function receives these arguments: RenderTexture, BlendTexture, final tick count, PosterSize, x coordinate, y coordinate
+		If BlendMode is a function and Finish is missing, BlendMode will be called instead.
+	Textures = The type of render targets to use. Can be one of these strings (case-insensitive):
+		* "basic" - The default RGB888 render target. This is the default value, and is the only type to support anti-aliasing (see this GitHub issue: https://github.com/Facepunch/garrysmod-issues/issues/2106)
+		* "int" - RGBA16161616 render target.
+		* "float" - RGBA16161616F render target.
+		Alternatively, it can be a table with two ITexture elements:
+			* Render = The texture rendered into in Tick
+			* Blend = The texture to hold the blend of all rendered ticks thus far.
+		It is not recommended to use the table method though, as it disables many of HeavyLight's tricks. If there is another type of render target that you need, you should make a request so I add it.
+	BlendMode = The method used to blend all Ticks together. Can be one of these strings (case-insensitive):
+		* "blend" = (Default) An average blend of all the ticks will be computed, with all ticks receiving equal weights. On the technical side, this is implemented by additive blending and finalized by dividing the values by the number of ticks.
+		* "blend2" = Same as blend, but implemented differently - instead of additive and division, a 'running average' is used.
+		* "add" / "additive" = Additive blending of all ticks.
+		Alternatively, it can be a function that receives these arguments: RenderTexture, BlendTexture, tick number, is this the last tick (boolean), PosterSize, x coordinate, y coordinate
+		The function will be called after every Tick that returns true, and after Tick returns false if Finish is missing, and is expected to blend into BlendTexture the ticks which are rendered into RenderTexture.
 }
 --]]
-
---- Starts a HeavyLight poster render.
--- @param poster_size The first parameter of the 'poster' console command.
--- @param poster_split The second parameter of the 'poster' console command.
--- @param new_section_func A function to be called at the beginning of each section of the poster. Can be nil if this isn't needed.
---	For example, if poster_size is 3, new_section_func will be called 9 times.
---	This function will receive 3 parameters - poster_size, x coordinate, and y coordinate.
--- @param tick_func The function to be called immediately after new_section_func, and repeatedly as long as it returns true.
---	This function will receive 4 parameters - poster_size, x coordinate, y coordinate, and tick count (per section).
--- @param render_target The render target type to use. Can either be a string or a table.
---	As a string it can be one of these (case-insensitive):
---	* "basic" - RGB888 render target. This is the only type to support anti-aliasing (https://github.com/Facepunch/garrysmod-issues/issues/2106)
---	* "int" - RGBA16161616 render target.
---	* "float" - RGBA16161616F render target. This is the default value.
---	As a table, it must be an array with two ITexture elements. The first element is the texture to be rendered into in tick_func, and the second is the texture to hold the blend of all rendered ticks thus far.
--- @param blend_mode The blend mode to use.
---	Can be one of the following (strings are case-insensitive):
---	* "blend" - Blend all rendered ticks together with 
-function StartPoster(poster_size, poster_split, renderdef)
-	if type(tick_func) ~= "function" then
-		error("",number errorLevel=1)
+local function CheckValidity(renderdef, errorlevel)
+	if type(renderdef.Tick) ~= "function" then
+		error("Missing Tick function for HeavyLight", (errorlevel or 2) + 1)
 	end
 end
+
+function Render(renderdef, drawprogress)
+	CheckValidity(renderdef)
+	GetRT()
+end
+
+local posterdata = {}
+--- Starts a HeavyLight poster render.
+-- @param renderdef A table of the structure defined someplace else. Give me a break okay?
+-- @param int postersize The first parameter of the 'poster' console command.
+-- @param[opt] int postersplit The second parameter of the 'poster' console command.
+function StartPoster(renderdef, postersize, postersplit)
+	CheckValidity(renderdef)
+	assert(tonumber(postersize), "bad argument #2 to StartPoster (number expected, got " .. type(postersize) .. ")")
+end
+
+hook.Add("RenderScene","HeavyLight",function()
+	if not posterdata.active then return end
+
+	Render(posterdata.renderdef, true)
+	return true
+end)
